167ef62dec28823e1ca8007233f961b2be3a3281
erwin antonius <erwin.ant@gmail.com>
2024-01-09 13:33:08 +0700

    Update envparser.js

--- src/envparser.js
+++ src/envparser.js
@@ -3,7 +3,7 @@ const dotEnv = require('dotenv');
 let parsedEnv = {};
 
 if (process.env.NODE_ENV === 'development') {
-    //parsedEnv = dotEnv.config({ path: '.env' }).parsed;
+    parsedEnv = dotEnv.config({ path: '.env' }).parsed;
 }
 
 module.exports = () => parsedEnv;
